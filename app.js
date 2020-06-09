0f2bc4d1d7f081e2b4d5e97007850739f0032bdd
vinny-sri <vinny.sri@berkeley.edu>
2020-06-09 04:14:54 -0700

    Fixed score count and text output

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 //Caching the DOM: Storing parts of the HTML as variables for ease of future reference
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 
 //Use .getElementById("id") to get a specific element given an ID in HTML
 //Use .querySelector() for anything not given by a specific ID --> returns the first instance
 const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("computer-score");
+const computerScore_span = document.getElementById("comp-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_div = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -35,17 +35,17 @@ function game(userChoice) {
     case "rs":
     case "sp":
     case "pr":
-      console.log("user wins");
+      win(userChoice, compChoice);
       break;
     case "sr":
     case "rp":
     case "ps":
-      console.log("computer wins");
+      lose(userChoice, compChoice);
       break;
     case "rr":
     case "pp":
     case "ss":
-      console.log("it's a draw");
+      draw(userChoice, compChoice);
       break;
   }
 }
@@ -54,4 +54,43 @@ function getComputerChoice() {
   const choices = ["r", "p", "s"];
   const randNum = Math.floor(Math.random() * 3);
   return choices[randNum];
+}
+
+function win(userChoice, compChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_div.innerHTML = format(userChoice, compChoice) + ". You win!";
+}
+
+function lose(userChoice, compChoice) {
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_div.innerHTML = format(userChoice, compChoice) + ". You lose!";
+}
+
+function draw(userChoice, compChoice) {
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_div.innerHTML = format(userChoice, compChoice) + ". It's a draw!";
+}
+
+function convert(letter) {
+  if (letter === "r") {
+    return "rock";
+  } else if (letter === "p") {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function format(userChoice, compChoice) {
+  return (
+    "You chose " +
+    convert(userChoice) +
+    " and the computer chose " +
+    convert(compChoice)
+  );
 }
